644207eaf3edf0c260689ba286c0482ddf0aac2b
jedlimlx <jedlimlx@outlook.com>
2025-03-18 09:12:14 +0800

    Fix issues with loading resource-packs

---
+++ src/main/java/io/github/jedlimlx/supplemental_patches/mixins/TextureAtlasMixin.java
@@ -0,0 +1,20 @@
+package io.github.jedlimlx.supplemental_patches.mixins;
+
+import io.github.jedlimlx.supplemental_patches.TextureStitchEvent;
+import net.minecraft.client.renderer.texture.SpriteLoader;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TextureAtlas.class)
+public class TextureAtlasMixin {
+    @Inject(
+        method = "upload",
+        at = @At("RETURN")
+    )
+    public void upload(SpriteLoader.Preparations preparations, CallbackInfo ci) {
+        TextureStitchEvent.Companion.getEVENT().invoker().afterStitched((TextureAtlas) (Object) this);
+    }
+}
---
+++ src/main/kotlin/io/github/jedlimlx/supplemental_patches/shaders/BiomeUniforms.kt
@@ -0,0 +1,27 @@
+package io.github.jedlimlx.supplemental_patches.shaders
+
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap
+import net.minecraft.client.Minecraft
+import net.minecraft.client.multiplayer.ClientPacketListener
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceKey
+import net.minecraft.world.level.biome.Biome
+
+val _biomeMap: Object2IntOpenHashMap<ResourceKey<Biome>> = Object2IntOpenHashMap()
+
+// handling biomes
+fun getBiomeMap(): Object2IntOpenHashMap<ResourceKey<Biome>> {
+    _biomeMap.clear()
+
+    val connection: ClientPacketListener? = Minecraft.getInstance()?.connection
+    if (connection != null) {
+        val biomes = connection.registryAccess().registry(Registries.BIOME).orElseThrow()
+
+        var currentId = 0
+        biomes.entrySet().sortedWith(
+            compareBy({ it.key.location().path }, { it.key.location().namespace })
+        ).forEach { _biomeMap[it.key] = currentId++ }
+    }
+
+    return _biomeMap
+}
