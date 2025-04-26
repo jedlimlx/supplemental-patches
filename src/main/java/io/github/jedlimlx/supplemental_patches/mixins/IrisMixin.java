29c910a45bb049fba5ccbfe888388ea69aac6e74
jedlimlx <jedlimlx@outlook.com>
2025-04-26 21:35:10 +0800

    Force Iris to load shaderpack later on when the mods list is available

---
+++ src/main/java/io/github/jedlimlx/supplemental_patches/mixins/IrisMixin.java
@@ -0,0 +1,18 @@
+package io.github.jedlimlx.supplemental_patches.mixins;
+
+import net.irisshaders.iris.Iris;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = Iris.class, remap = false)
+public class IrisMixin {
+    @Redirect(
+        method = "onRenderSystemInit",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/irisshaders/iris/Iris;loadShaderpack()V"
+        )
+    )
+    private static void method() {}
+}
--- src/main/kotlin/io/github/jedlimlx/supplemental_patches/events/ClientEvents.kt
+++ src/main/kotlin/io/github/jedlimlx/supplemental_patches/events/ClientEvents.kt
@@ -3,6 +3,7 @@ package io.github.jedlimlx.supplemental_patches.events
 import io.github.jedlimlx.supplemental_patches.MODID
 import io.github.jedlimlx.supplemental_patches.SupplementalPatches
 import io.github.jedlimlx.supplemental_patches.shaders.installShader
+import net.irisshaders.iris.Iris
 import net.minecraft.client.Minecraft
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.client.event.TextureStitchEvent
@@ -11,11 +12,12 @@ import net.minecraftforge.fml.common.Mod
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = MODID, value = [Dist.CLIENT])
 object ClientEvents {
-    // TODO reload shaders after mods are loaded, if possible
     @SubscribeEvent
     fun textureStitchedEvent(event: TextureStitchEvent.Post) {
         val textureAtlas = Minecraft.getInstance().particleEngine.textureAtlas
-        if (event.atlas.location() == textureAtlas.location())
+        if (event.atlas.location() == textureAtlas.location()) {
             SupplementalPatches.LOGGER.info(installShader())
+            Iris.loadShaderpackWhenPossible()
+        }
     }
 }
