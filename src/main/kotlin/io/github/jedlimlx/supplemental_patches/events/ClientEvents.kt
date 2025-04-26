588f7f0ad8bb1c4ae52fa39f523344395c37833b
jedlimlx <jedlimlx@outlook.com>
2025-04-26 21:35:10 +0800

    Force Iris to load shaderpack later on when the mods list is available

--- src/main/kotlin/io/github/jedlimlx/supplemental_patches/events/ClientEvents.kt
+++ src/main/kotlin/io/github/jedlimlx/supplemental_patches/events/ClientEvents.kt
@@ -3,6 +3,7 @@ package io.github.jedlimlx.supplemental_patches.events
 import io.github.jedlimlx.supplemental_patches.MODID
 import io.github.jedlimlx.supplemental_patches.SupplementalPatches
 import io.github.jedlimlx.supplemental_patches.shaders.installShader
+import net.irisshaders.iris.Iris
 import net.minecraft.client.Minecraft
 import net.neoforged.api.distmarker.Dist
 import net.neoforged.bus.api.SubscribeEvent
@@ -11,11 +12,12 @@ import net.neoforged.neoforge.client.event.TextureAtlasStitchedEvent
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, modid = MODID, value = [Dist.CLIENT])
 object ClientEvents {
-    // TODO reload shaders after mods are loaded, if possible
     @SubscribeEvent
     fun textureStitchedEvent(event: TextureAtlasStitchedEvent) {
         val textureAtlas = Minecraft.getInstance().particleEngine.textureAtlas
-        if (event.atlas.location() == textureAtlas.location())
+        if (event.atlas.location() == textureAtlas.location()) {
             SupplementalPatches.LOGGER.info(installShader())
+            Iris.loadShaderpackWhenPossible()
+        }
     }
 }
