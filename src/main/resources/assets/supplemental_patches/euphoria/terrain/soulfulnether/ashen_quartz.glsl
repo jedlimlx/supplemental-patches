65b64e11217277d2c593f60966b067c2ef6dac26
jedlimlx <jedlimlx@outlook.com>
2025-04-30 19:37:02 +0800

    Add support for Soulful Nether and fix ACL issues for translucent materials

---
+++ src/main/resources/assets/supplemental_patches/euphoria/terrain/soulfulnether/ashen_quartz.glsl
@@ -0,0 +1,2 @@
+#include "/lib/materials/specificMaterials/terrain/quartzBlock.glsl"
+#include "/lib/materials/specificMaterials/terrain/iridescentAsh.glsl"
---
+++ src/main/resources/assets/supplemental_patches/euphoria/terrain/soulfulnether/vines.glsl
@@ -0,0 +1,3 @@
+subsurfaceMode = 1;
+
+sandNoiseIntensity = 0.0, mossNoiseIntensity = 0.0, isFoliage = true;
