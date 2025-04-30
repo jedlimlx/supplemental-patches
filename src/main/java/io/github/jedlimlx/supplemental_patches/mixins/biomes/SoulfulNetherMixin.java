e1c636deef9c95cbe4095bdd97860ad76cfb5ddb
jedlimlx <jedlimlx@outlook.com>
2025-04-30 19:37:02 +0800

    Add support for Soulful Nether and fix ACL issues for translucent materials

---
+++ src/main/java/io/github/jedlimlx/supplemental_patches/mixins/biomes/SoulfulNetherMixin.java
@@ -0,0 +1,21 @@
+package io.github.jedlimlx.supplemental_patches.mixins.biomes;
+
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import net.bon.soulfulnether.worldgen.SoulfulBiomes;
+import net.irisshaders.iris.uniforms.BiomeUniforms;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Restriction(require = @Condition("soulfulnether"))
+@Mixin(value = SoulfulBiomes.class, remap = false)
+public class SoulfulNetherMixin {
+    @Inject(method = "register", at = @At("TAIL"))
+    private static void registerBiome(String string, CallbackInfoReturnable<ResourceKey<Biome>> cir) {
+        BiomeUniforms.getBiomeMap().put(cir.getReturnValue(), BiomeUniforms.getBiomeMap().size() + 1);
+    }
+}
