4cd3455dbe5b9cc3ac7d2d476ee04b371715028a
jedlimlx <jedlimlx@outlook.com>
2025-05-03 10:53:30 +0800

    Add new uniform for Betrayed effect

---
+++ src/main/java/io/github/jedlimlx/supplemental_patches/mixins/uniforms/JNEUniforms.java
@@ -0,0 +1,43 @@
+package io.github.jedlimlx.supplemental_patches.mixins.uniforms;
+
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import net.irisshaders.iris.gl.uniform.UniformHolder;
+import net.irisshaders.iris.gl.uniform.UniformUpdateFrequency;
+import net.irisshaders.iris.shaderpack.properties.PackDirectives;
+import net.irisshaders.iris.uniforms.CommonUniforms;
+import net.irisshaders.iris.uniforms.FrameUpdateNotifier;
+import net.jadenxgamer.netherexp.registry.effect.JNEMobEffects;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Restriction(require = @Condition("netherexp"))
+@Mixin(CommonUniforms.class)
+public class JNEUniforms {
+    @Inject(
+        method = "generalCommonUniforms(Lnet/irisshaders/iris/gl/uniform/UniformHolder;Lnet/irisshaders/iris/uniforms/FrameUpdateNotifier;Lnet/irisshaders/iris/shaderpack/properties/PackDirectives;)V",
+        at = @At("TAIL"),
+        remap = false
+    )
+    private static void generalCommonUniforms(UniformHolder uniforms, FrameUpdateNotifier updateNotifier, PackDirectives directives, CallbackInfo ci) {
+        uniforms.uniform1f(UniformUpdateFrequency.PER_FRAME, "betrayed", JNEUniforms::getBetrayedEffect);
+    }
+
+    private static float getBetrayedEffect() {
+        Entity cameraEntity = Minecraft.getInstance().getCameraEntity();
+        if (cameraEntity instanceof LivingEntity) {
+            MobEffectInstance betrayed = ((LivingEntity)cameraEntity).getEffect(JNEMobEffects.BETRAYED.get());
+            if (betrayed != null) {
+                return betrayed.getAmplifier();
+            } else return 0.0F;
+        }
+
+        return 0.0F;
+    }
+}
