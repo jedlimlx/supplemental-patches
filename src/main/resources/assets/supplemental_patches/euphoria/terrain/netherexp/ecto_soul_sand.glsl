bedd122a38ef547dff62fbc388d4c4098e71d32c
jedlimlx <jedlimlx@outlook.com>
2025-05-03 10:53:30 +0800

    Add new uniform for Betrayed effect

--- src/main/resources/assets/supplemental_patches/euphoria/terrain/netherexp/ecto_soul_sand.glsl
+++ src/main/resources/assets/supplemental_patches/euphoria/terrain/netherexp/ecto_soul_sand.glsl
@@ -3,5 +3,6 @@ smoothnessD = color.r * 0.25;
 
 if (color.b > color.r) {
     emission = pow1_5(max0(2.0 * color.b - color.r)) + 0.1;
+    emission *= mix(1.0, 0.0, min1(lViewPos / 64.0));  // fade-off into the distance
     color.rgb = pow1_5(color.rgb);
 }
