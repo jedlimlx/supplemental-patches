a28d5478147cf7977ec638e2fe132899ef41ad2e
jedlimlx <jedlimlx@outlook.com>
2025-06-01 15:45:59 +0800

    Fix issues with settings not appearing and implement Enderscape nebula settings

--- src/main/resources/assets/supplemental_patches/euphoria/terrain/soulfulnether/fright_stem.glsl
+++ src/main/resources/assets/supplemental_patches/euphoria/terrain/soulfulnether/fright_stem.glsl
@@ -5,11 +5,13 @@ if (abs(color.b - color.g) < 0.07 && color.b > 0.35) { // Flat Part
 } else { // Emissive Part
     emission = pow2(color.b) * 5.0 + 0.15;
     overlayNoiseIntensity = 0.7, overlayNoiseEmission = 0.3;
-    #if defined GBUFFERS_TERRAIN && defined ANIMATED_FRIGHT_STEM
-        vec3 wind = vec3(0, 0.0075, 0) * frameTimeCounter;
-        float noise = Noise3D(0.01 * (playerPos.xyz + cameraPosition.xyz) + wind);
-        emission *= 0.1 + 0.9 * pow2(noise);
-        emission *= 4.0;
+    #ifdef ANIMATED_FRIGHT_STEM
+        #if defined GBUFFERS_TERRAIN
+            vec3 wind = vec3(0, 0.0075, 0) * frameTimeCounter;
+            float noise = Noise3D(0.01 * (playerPos.xyz + cameraPosition.xyz) + wind);
+            emission *= 0.1 + 0.9 * pow2(noise);
+            emission *= 4.0;
+        #endif
     #endif
     #endif
 }
