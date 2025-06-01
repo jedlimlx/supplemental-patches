e520da4f4b479e05556f7c2e229eb490bba87211
jedlimlx <jedlimlx@outlook.com>
2025-06-01 15:45:59 +0800

    Fix issues with settings not appearing and implement Enderscape nebula settings

--- src/main/resources/assets/supplemental_patches/euphoria/terrain/enderscape/celestial_growth.glsl
+++ src/main/resources/assets/supplemental_patches/euphoria/terrain/enderscape/celestial_growth.glsl
@@ -1,8 +1,9 @@
 subsurfaceMode = 1, noDirectionalShading = true, isFoliage = true;
 
-if (color.r > 0.8) {
+#ifdef GLOWING_CELESTIAL_GROWTHS
+if (color.r > 0.8)
     emission = 0.9 * sqrt(color.r);
-}
+#endif
 
 #ifdef COATED_TEXTURES
     noiseFactor = 0.66;
