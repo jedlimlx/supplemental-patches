d3da7068f52176fb4481b8031c241ed4b07d70b9
jedlimlx <jedlimlx@outlook.com>
2025-06-03 13:26:43 +0800

    Improvements and fixes to Enderscape atmosphere

--- src/main/resources/assets/supplemental_patches/euphoria/mixins/enderscape/darker_end_sky.glsl
+++ src/main/resources/assets/supplemental_patches/euphoria/mixins/enderscape/darker_end_sky.glsl
@@ -1,5 +1,3 @@
 #ifdef DARKER_END_SKY
-    const vec3 originalEndSkyColor = vec3(E_SKY_COLORR, E_SKY_COLORG, E_SKY_COLORB) / 255.0 * E_SKY_COLORI * 0.50;
-#else
-    const vec3 originalEndSkyColor = vec3(E_SKY_COLORR, E_SKY_COLORG, E_SKY_COLORB) / 255.0 * E_SKY_COLORI;
+    endAtmMult *= 0.5;
 #endif
