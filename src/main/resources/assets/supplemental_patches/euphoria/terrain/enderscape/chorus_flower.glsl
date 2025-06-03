e43576d565a27bdaa389ef4f9404fa6c7d8e7e24
jedlimlx <jedlimlx@outlook.com>
2025-06-03 13:26:43 +0800

    Improvements and fixes to Enderscape atmosphere

--- src/main/resources/assets/supplemental_patches/euphoria/terrain/enderscape/chorus_flower.glsl
+++ src/main/resources/assets/supplemental_patches/euphoria/terrain/enderscape/chorus_flower.glsl
@@ -1,7 +1,15 @@
-#ifdef GLOWING_CHROUS_FLOWER
-    float dotColor = dot(color.rgb, color.rgb);
-    if (dotColor > 1.0) {
-        emission = pow2(pow2(pow2(dotColor * 0.33))) + 0.2 * dotColor;
-        overlayNoiseIntensity = 0.5, overlayNoiseEmission = 0.6;
-    }
+#ifndef NOT_GLOWING_CHORUS_FLOWER
+    vec3 checkColor = texture2DLod(tex, texCoord, 0).rgb;
+    if (
+        CheckForColor(checkColor, vec3(164, 157, 126)) ||
+        CheckForColor(checkColor, vec3(201, 197, 176)) ||
+        CheckForColor(checkColor, vec3(226, 221, 188)) ||
+        CheckForColor(checkColor, vec3(153, 142, 95))
+    ) {
+        emission = min(GetLuminance(color.rgb), 0.75) / 0.75;
+        emission = pow2(pow2(emission)) * 6.5;
+        color.gb *= 0.85;
+
+        overlayNoiseIntensity = 0.1, overlayNoiseEmission = 0.8;
+    } else emission = max0(GetLuminance(color.rgb) - 0.5) * 3.0;
 #endif
