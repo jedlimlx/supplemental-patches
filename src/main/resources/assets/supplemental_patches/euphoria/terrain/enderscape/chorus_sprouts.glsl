589126fcf27bc94aa5c3084a61d313d0443390d7
jedlimlx <jedlimlx@outlook.com>
2025-06-03 13:26:43 +0800

    Improvements and fixes to Enderscape atmosphere

--- src/main/resources/assets/supplemental_patches/euphoria/terrain/enderscape/chorus_sprouts.glsl
+++ src/main/resources/assets/supplemental_patches/euphoria/terrain/enderscape/chorus_sprouts.glsl
@@ -1,7 +1,7 @@
 subsurfaceMode = 1, noDirectionalShading = true, isFoliage = true;
 
-#ifdef GLOWING_CHORUS_FLOWER
+#ifndef NOT_GLOWING_CHORUS_FLOWER
     if (color.r < color.g * 1.2 || color.r > 0.78) {
-        emission = 1.0;
+        emission = 1.2;
     }
 #endif
