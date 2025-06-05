c66320098beea4150866cb9f61eef309c1f365f3
jedlimlx <jedlimlx@outlook.com>
2025-06-05 23:51:25 +0800

    Merge 3 uniforms for Enderscape nebula colours into one

--- src/main/java/io/github/jedlimlx/supplemental_patches/mixins/uniforms/EnderscapeAtmosphereUniforms.java
+++ src/main/java/io/github/jedlimlx/supplemental_patches/mixins/uniforms/EnderscapeAtmosphereUniforms.java
@@ -3,33 +3,25 @@ package io.github.jedlimlx.supplemental_patches.mixins.uniforms;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.bunten.enderscape.biome.util.SkyParameters;
-import net.irisshaders.iris.gl.uniform.FloatSupplier;
 import net.irisshaders.iris.gl.uniform.UniformHolder;
 import net.irisshaders.iris.gl.uniform.UniformUpdateFrequency;
 import net.irisshaders.iris.uniforms.BiomeUniforms;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.util.ToFloatFunction;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.awt.*;
 import java.util.Optional;
-import java.util.function.IntSupplier;
-import java.util.function.ToIntFunction;
 
 import static net.bunten.enderscape.registry.EnderscapeBiomes.DEFAULT_NEBULA_ALPHA;
 import static net.bunten.enderscape.registry.EnderscapeBiomes.DEFAULT_NEBULA_COLOR;
 @Restriction(require = @Condition("enderscape"))
 @Mixin(value = BiomeUniforms.class, remap = false)
 public abstract class EnderscapeAtmosphereUniforms {
-    @Shadow
-    static IntSupplier playerI(ToIntFunction<LocalPlayer> function) { return () -> 0; }
-
     private static Color getNebulaColor(LocalPlayer player) {
         Optional<SkyParameters> temp = SkyParameters.getSkyParametersFor(player.level().getBiome(player.blockPosition()));
         return temp.map(skyParameters -> new Color(skyParameters.nebulaColor())).orElse(new Color(DEFAULT_NEBULA_COLOR));
@@ -41,24 +33,16 @@ public abstract class EnderscapeAtmosphereUniforms {
         remap = false
     )
     private static void addBiomeUniforms(UniformHolder uniforms, CallbackInfo ci) {
-        uniforms.uniform1f(
+        uniforms.uniform3f(
             UniformUpdateFrequency.PER_TICK,
-            "enderscapeNebulaRed",
-            playerI(
-                (player) -> getNebulaColor(player).getRed()
-            )
-        ).uniform1f(
-            UniformUpdateFrequency.PER_TICK,
-            "enderscapeNebulaGreen",
-            playerI(
-                (player) -> getNebulaColor(player).getGreen()
-            )
-        ).uniform1f(
-            UniformUpdateFrequency.PER_TICK,
-            "enderscapeNebulaBlue",
-            playerI(
-                (player) -> getNebulaColor(player).getBlue()
-            )
+            "enderscapeNebulaColor",
+            () -> {
+                LocalPlayer player = Minecraft.getInstance().player;
+                Color color;
+                if (player != null) color = getNebulaColor(player);
+                else color = new Color(DEFAULT_NEBULA_COLOR);
+                return new Vector3f(color.getRed(), color.getGreen(), color.getBlue());
+            }
         ).uniform1f(
             UniformUpdateFrequency.PER_TICK,
             "enderscapeNebulaAlpha",
