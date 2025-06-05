6ff3a87283c73d45a82b6663490c970e09234e45
jedlimlx <jedlimlx@outlook.com>
2025-06-05 21:27:50 +0800

    Improve shaders for Biomes O Plenty

--- src/main/resources/assets/supplemental_patches/euphoria/terrain/biomesoplenty/burning_blossom.glsl
+++ src/main/resources/assets/supplemental_patches/euphoria/terrain/biomesoplenty/burning_blossom.glsl
@@ -5,6 +5,7 @@ if (color.r > 0.78 || color.r - color.g > 0.1) {
     color.rgb *= color.rgb;
     #ifdef GBUFFERS_TERRAIN
         lmCoordM.x += 0.5 - 0.7 * pow2(length(signMidCoordPos));
+        lmCoordM.x = min1(lmCoordM.x);
     #endif
 }
 
