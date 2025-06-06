62c5ae2e831a932a7e956bfb0b1a7c13ed66d70a
jedlimlx <jedlimlx@outlook.com>
2025-06-06 16:55:17 +0800

    Add option to allow nebulae to change its colour based on the current biome

---
+++ src/main/java/io/github/jedlimlx/supplemental_patches/mixins/biomes/EnderscapeBiomesMixin.java
@@ -0,0 +1,21 @@
+package io.github.jedlimlx.supplemental_patches.mixins.biomes;
+
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import net.bunten.enderscape.registry.EnderscapeBiomes;
+import net.irisshaders.iris.uniforms.BiomeUniforms;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Restriction(require = @Condition("enderscape"))
+@Mixin(value = EnderscapeBiomes.class, remap = false)
+public class EnderscapeBiomesMixin {
+    @Inject(method = "register", at = @At("TAIL"))
+    private static void registerBiome(String string, CallbackInfoReturnable<ResourceKey<Biome>> cir) {
+        BiomeUniforms.getBiomeMap().put(cir.getReturnValue(), BiomeUniforms.getBiomeMap().size() + 1);
+    }
+}
