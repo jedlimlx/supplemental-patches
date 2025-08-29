2baf5b8e17ceb1708dfb2517f6f68bcc69ca9c27
jedlimlx <jedlimlx@outlook.com>
2025-08-29 14:13:10 +0800

    Allow enabling of Doom & Gloom Fog without having Doom & Gloom installed

--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/mixins/doom_and_gloom/aurora_visibility.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/mixins/doom_and_gloom/aurora_visibility.glsl
@@ -1,3 +1,5 @@
-#ifdef MOD_DOOM_AND_GLOOM
-    visibility *= (1 - FOG_AURORA_VISIBILITY) * (1 - doomAndGloomFog) + FOG_AURORA_VISIBILITY;
+#ifdef DO_DOOM_AND_GLOOM_FOG
+    visibility *= FOG_AURORA_VISIBILITY;
+#elif MOD_DOOM_AND_GLOOM
+    visibility *= (1 - FOG_AURORA_VISIBILITY) * (1 - doomAndGloomFog) + FOG_AURORA_VISIBILITY * doomAndGloomFog;
 #endif
