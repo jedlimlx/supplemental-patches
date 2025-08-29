b05d0e82c050a6ba94a96009bde17f3a5902db44
jedlimlx <jedlimlx@outlook.com>
2025-08-29 14:13:10 +0800

    Allow enabling of Doom & Gloom Fog without having Doom & Gloom installed

--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/mixins/doom_and_gloom/less_sunlight.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/mixins/doom_and_gloom/less_sunlight.glsl
@@ -1,4 +1,6 @@
-#ifdef MOD_DOOM_AND_GLOOM
+#ifdef DO_DOOM_AND_GLOOM_FOG
+    float lightDimming = FOG_LIGHT_DIMMING;
+#elif MOD_DOOM_AND_GLOOM
     float lightDimming = mix(1.0, FOG_LIGHT_DIMMING, doomAndGloomFog);
 #else
     float lightDimming = 1.0;
