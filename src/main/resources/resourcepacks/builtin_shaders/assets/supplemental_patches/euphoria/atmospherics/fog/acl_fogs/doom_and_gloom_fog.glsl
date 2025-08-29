e3ebb22c747def9ce57be7dba11bb531b8938aa5
jedlimlx <jedlimlx@outlook.com>
2025-08-29 14:13:10 +0800

    Allow enabling of Doom & Gloom Fog without having Doom & Gloom installed

--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/atmospherics/fog/acl_fogs/doom_and_gloom_fog.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/atmospherics/fog/acl_fogs/doom_and_gloom_fog.glsl
@@ -1,4 +1,7 @@
-#ifdef MOD_DOOM_AND_GLOOM
+#ifdef DO_DOOM_AND_GLOOM_FOG
+    lightSample *= max(0.05, 1.0 - lTracePos * 0.03);
+    lightSample *= vec3(1.2, 0.7, 0.3);  // reddish light cuts through fog better
+#elif MOD_DOOM_AND_GLOOM
     lightSample *= max(0.05, 1.0 - lTracePos * 0.03 * doomAndGloomFog);
     lightSample *= vec3(1.2, 0.7, 0.3);  // reddish light cuts through fog better
 #endif
