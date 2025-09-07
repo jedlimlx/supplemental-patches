1d04cc4a4f788b290fbc8ac7abdaf5a52b21bdbf
jedlimlx <jedlimlx@outlook.com>
2025-09-07 09:18:54 +0800

    Fix issue with minecraft namespace not replacing IDs properly

--- src/main/kotlin/io/github/jedlimlx/supplemental_patches/shaders/Util.kt
+++ src/main/kotlin/io/github/jedlimlx/supplemental_patches/shaders/Util.kt
@@ -135,7 +135,8 @@ fun _computeAllPivots(lst: List<Int>, depth: Int, variable: String, builder: Str
 
 // removing id
 fun removeId(id: String, string: String): String {
-    val newString = Regex("$id( |\\r\\n|\\n|\\r)").replace(string, "")
+    val id = id.replace("minecraft:", "")
+    val newString = Regex("(minecraft:)?$id( |\\r\\n|\\n|\\r)").replace(string, "")
     return Regex("\\\\( |\\r\\n|\\n|\\r)\\\\( |\\r\\n|\\n|\\r)( |\\r\\n|\\n|\\r)").replace(newString, "")
 }
 
