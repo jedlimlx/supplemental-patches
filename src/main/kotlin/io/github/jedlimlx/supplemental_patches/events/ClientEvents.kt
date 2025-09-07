c737ce0663ea35c9b44bda30ce431b9b88072175
jedlimlx <jedlimlx@outlook.com>
2025-09-07 14:55:56 +0800

    Add message when shaders are installed on resource-pack reload

--- src/main/kotlin/io/github/jedlimlx/supplemental_patches/events/ClientEvents.kt
+++ src/main/kotlin/io/github/jedlimlx/supplemental_patches/events/ClientEvents.kt
@@ -22,7 +22,9 @@ object ClientEvents {
     fun textureStitchedEvent(event: TextureAtlasStitchedEvent) {
         val textureAtlas = Minecraft.getInstance().particleEngine.textureAtlas
         if (event.atlas.location() == textureAtlas.location()) {
-            SupplementalPatches.LOGGER.info(installShader())
+            val string = installShader()
+            SupplementalPatches.LOGGER.info(string)
+            Minecraft.getInstance().player?.sendSystemMessage(Component.nullToEmpty(string))
             Iris.loadShaderpackWhenPossible()
         }
     }
