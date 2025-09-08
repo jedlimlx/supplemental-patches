e058153932c33adfabcf99f00126278f05523153
jedlimlx <jedlimlx@outlook.com>
2025-09-08 09:24:16 +0800

    Replace `endstone` with `endStone` so shaders will work with case-sensitive file systems (thanks @lightdrew)

--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/terrain/endergetic/eumus.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/terrain/endergetic/eumus.glsl
@@ -2,7 +2,7 @@ if (mat % 4 < 2) {
     smoothnessG = color.r * 0.1 + 0.1;
     smoothnessD = smoothnessG;
     if (color.g > 0.5) {
-        #include "/lib/materials/specificMaterials/terrain/endstone.glsl"
+        #include "/lib/materials/specificMaterials/terrain/endStone.glsl"
     }
 } else {  // bricks
     float factor = 1.5 * smoothstep1(clamp01(color.r * 2.5));
