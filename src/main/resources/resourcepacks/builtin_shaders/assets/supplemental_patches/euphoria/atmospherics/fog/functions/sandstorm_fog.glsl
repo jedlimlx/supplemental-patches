e3277b5d4d0d18599aba9307830ac91cd6eed503
chililisoup <rebot333gaming@gmail.com>
2025-09-21 15:52:07 -0600

    avoid undefined yungSandstormFactor

--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/atmospherics/fog/functions/sandstorm_fog.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/atmospherics/fog/functions/sandstorm_fog.glsl
@@ -1,7 +1,9 @@
 void DoSandstormFog(inout vec3 color, float lViewPos) {
-    float fog = lViewPos * yungSandstormFactor;
-    fog = sqrt(fog) * YUNGS_SANDSTORM_FOG_INTENSITY;
-    fog = 1.0 - exp(-fog);
+    #ifdef MOD_YUNGSCAVEBIOMES
+        float fog = lViewPos * yungSandstormFactor;
+        fog = sqrt(fog) * YUNGS_SANDSTORM_FOG_INTENSITY;
+        fog = 1.0 - exp(-fog);
 
-    color = mix(color, vec3(0.8, 0.5, 0.1), fog);
+        color = mix(color, vec3(0.8, 0.5, 0.1), fog);
+    #endif
 }
