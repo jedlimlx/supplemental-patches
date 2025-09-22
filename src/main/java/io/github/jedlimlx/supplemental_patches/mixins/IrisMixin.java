cc413ddcde138ae96c356860de24978afe21599a
jedlimlx <jedlimlx@outlook.com>
2025-09-22 15:14:26 +0800

    Print Iris errors to the Minecraft chat

--- src/main/java/io/github/jedlimlx/supplemental_patches/mixins/IrisMixin.java
+++ src/main/java/io/github/jedlimlx/supplemental_patches/mixins/IrisMixin.java
@@ -3,6 +3,7 @@ package io.github.jedlimlx.supplemental_patches.mixins;
 import net.irisshaders.iris.Iris;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = Iris.class, remap = false)
