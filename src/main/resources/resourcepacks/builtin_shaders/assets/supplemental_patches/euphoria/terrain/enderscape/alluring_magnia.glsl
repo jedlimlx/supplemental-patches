0253fe64bc6d89b2b480caf5c14306dfd476c05d
jedlimlx <jedlimlx@outlook.com>
2025-09-27 00:07:27 +0800

    Update Enderscape to 1.1.0

--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/terrain/enderscape/alluring_magnia.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/terrain/enderscape/alluring_magnia.glsl
@@ -1,4 +1,11 @@
 if (mat % 4 < 3) {
+    if (color.r > color.g * 5.0) {
+        emission = 8.0 * color.r;
+        color.rgb *= color.rgb;
+
+        overlayNoiseIntensity = 0.35, overlayNoiseEmission = 0.2;
+    }
+
     if (color.b < 0.99) {
         #include "/lib/materials/specificMaterials/terrain/alluringMagnia.glsl"
     } else {
