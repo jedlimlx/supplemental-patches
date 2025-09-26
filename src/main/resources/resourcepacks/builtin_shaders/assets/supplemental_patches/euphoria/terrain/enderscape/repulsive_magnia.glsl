18069aad0859541cff2dbd04fbd804c4f8c55ecc
jedlimlx <jedlimlx@outlook.com>
2025-09-27 00:07:27 +0800

    Update Enderscape to 1.1.0

--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/terrain/enderscape/repulsive_magnia.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/terrain/enderscape/repulsive_magnia.glsl
@@ -1,4 +1,11 @@
 if (mat % 4 < 3) {
+    if (color.r > color.g * 5.0) {
+        emission = 8.0 * color.r;
+        color.rgb *= color.rgb;
+
+        overlayNoiseIntensity = 0.35, overlayNoiseEmission = 0.2;
+    }
+
     if (color.r < 0.8) {
         #include "/lib/materials/specificMaterials/terrain/repulsiveMagnia.glsl"
     } else {
