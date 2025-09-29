bd73aecf2ebbd33056b13fb1d331943ee671a2a5
jedlimlx <jedlimlx@outlook.com>
2025-09-29 10:26:37 +0800

    Add support for Diamonds in the Rough and Beeten

---
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/items/beeten/heart_beet.glsl
@@ -0,0 +1,11 @@
+materialMask = OSIEBCA; // Intense Fresnel
+                                
+float factor = pow2(color.g);
+highlightMult = factor * 3.0;
+color.rgb *= 0.7 + 0.3 * GetLuminance(color.rgb);
+
+emission = dot(color.rgb, color.rgb) * 0.35 + 0.1;
+overlayNoiseEmission = 0.5;
+
+smoothnessG = 0.8 - factor * 0.3;
+smoothnessD = factor;
