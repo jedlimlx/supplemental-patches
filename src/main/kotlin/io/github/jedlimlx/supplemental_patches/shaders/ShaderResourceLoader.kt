972d278ee71d42e988be7ce93de090d35bb7e8b4
jedlimlx <jedlimlx@outlook.com>
2025-10-27 09:31:53 +0800

    Clear REFACTORS on resource reload

--- src/main/kotlin/io/github/jedlimlx/supplemental_patches/shaders/ShaderResourceLoader.kt
+++ src/main/kotlin/io/github/jedlimlx/supplemental_patches/shaders/ShaderResourceLoader.kt
@@ -110,6 +110,8 @@ object ShaderResourceLoader {
 
         BUFFERS.clear()
 
+        REFACTORS.clear()
+
         // Loading various colours
         val lst = resourceManager.listResources("euphoria/colors") { it.path.endsWith(".json") }
 
