0b0d53e7af7f80e402286154a4017acec2240e98
jedlimlx <jedlimlx@outlook.com>
2025-11-09 07:23:55 +0100

    Add support for Enderscape end flashes and refactor Enderscape shader settings

--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/mixins/enderscape/darker_end_atmosphere.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/mixins/enderscape/darker_end_atmosphere.glsl
@@ -1,3 +1,6 @@
-#ifdef DARKER_END_ATMOSPHERE
-    endLightMult *= 0.6 * vec3(0.6, 1.0, 0.6);
+#ifdef (DARKER_END_ATMOSPHERE == 1 && defined MOD_ENDERSCAPE) || DARKER_END_ATMOSPHERE == 2
+    vec3 ambientColorTint = 0.6 * vec3(0.6, 1.0, 0.6);
+#else
+    vec3 ambientColorTint = vec3(1.0);
 #endif
+vec3 ambientColor = ambientColorTint * mix(ambientCol, vec3(END_AMBIENT_R_NEW, END_AMBIENT_G_NEW, END_AMBIENT_B_NEW), END_AMBIENT_INFLUENCE) * END_AMBIENT_I;
--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/mixins/enderscape/darker_end_sky.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/mixins/enderscape/darker_end_sky.glsl
@@ -1,3 +1,3 @@
-#ifdef DARKER_END_SKY
+#ifdef (DARKER_END_SKY == 1 && defined MOD_ENDERSCAPE) || DARKER_END_SKY == 2
     endAtmMult *= 0.5;
 #endif
