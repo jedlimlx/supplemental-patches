246150c5ff7e3a06f98ca12a7e2e7c0e2c265239
jedlimlx <jedlimlx@outlook.com>
2025-11-16 16:31:20 +0800

    Fix some bugs with shaders

--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/mixins/doom_and_gloom/moon_blurring.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/mixins/doom_and_gloom/moon_blurring.glsl
@@ -1,1 +1,5 @@
-color.rgb = mix(color.rgb, vec3(0.5), doomAndGloomFog * pow2(acos(absVdotS) / acos(sunSizeFactor1)));
+#if DOOM_AND_GLOOM_FOG == 1
+    color.rgb = mix(color.rgb, vec3(0.5), pow2(acos(absVdotS) / acos(sunSizeFactor1)));
+#elif defined MOD_DOOM_AND_GLOOM && (DOOM_AND_GLOOM_FOG == 0)
+    color.rgb = mix(color.rgb, vec3(0.5), doomAndGloomFog * pow2(acos(absVdotS) / acos(sunSizeFactor1)));
+#endif
