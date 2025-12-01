00c09f767bbf00cbcc1c63cac05efe3b26f46224
jedlimlx <jedlimlx@outlook.com>
2025-12-01 20:25:30 +0800

    Adding new debug messages

--- src/main/kotlin/io/github/jedlimlx/supplemental_patches/shaders/ShaderResourceLoader.kt
+++ src/main/kotlin/io/github/jedlimlx/supplemental_patches/shaders/ShaderResourceLoader.kt
@@ -662,7 +662,10 @@ object ShaderResourceLoader {
     ): CompletableFuture<Void> {
         return CompletableFuture.supplyAsync (
             {
-                resourceManager.listResources(type) { it.path.endsWith(".json") }.forEachWithErrorHandling { (loc, _) ->
+                val lst = resourceManager.listResources(type) { it.path.endsWith(".json") }
+                LOGGER.info("Loading ${lst.size} custom textures...")
+
+                lst.forEachWithErrorHandling { (loc, _) ->
                     val json = loadJson(loc, resourceManager)
                     TEXTURES.add(
                         Texture(
@@ -681,7 +684,10 @@ object ShaderResourceLoader {
     ): CompletableFuture<Void> {
         return CompletableFuture.supplyAsync (
             {
-                resourceManager.listResources(type) { it.path.endsWith(".json") }.forEachWithErrorHandling { (loc, _) ->
+                val lst = resourceManager.listResources(type) { it.path.endsWith(".json") }
+                LOGGER.info("Loading ${lst.size} custom buffers...")
+
+                lst.forEachWithErrorHandling { (loc, _) ->
                     val json = loadJson(loc, resourceManager)
                     BUFFERS.add(
                         Buffer(
