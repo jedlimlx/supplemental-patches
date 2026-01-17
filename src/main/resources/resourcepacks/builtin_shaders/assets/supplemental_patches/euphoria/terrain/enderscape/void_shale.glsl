98aa5ba4b12e519bfaa896547d1479a28271ea43
jedlimlx <jedlimlx@outlook.com>
2026-01-17 17:10:35 +0800

    Port Enderscape to 2.1.0

--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/terrain/enderscape/void_shale.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/terrain/enderscape/void_shale.glsl
@@ -1,2 +1,4 @@
 #include "/lib/materials/specificMaterials/terrain/obsidian.glsl"
-emission = color.b > 0.27 ? 2.0 * color.b : 0.0;
+if (abs(NdotU) < 0.1) {
+    emission = color.b > 0.27 ? 2.0 * color.b : 0.0;
+}
