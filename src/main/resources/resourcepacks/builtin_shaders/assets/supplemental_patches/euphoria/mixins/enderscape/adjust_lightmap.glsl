b4ea2bdec2c26da8c277bcb0caff5c6516d6e626
jedlimlx <jedlimlx@outlook.com>
2026-01-23 23:49:52 +0800

    Allow adjusting Enderscape star colour

--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/mixins/enderscape/adjust_lightmap.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/mixins/enderscape/adjust_lightmap.glsl
@@ -1,6 +1,5 @@
 #if defined END && (defined ES_LIGHTMAP == 2 || (defined ES_LIGHTMAP == 1 && defined MOD_ENDERSCAPE))
-    vec3 blockLighting = lightmapXM * blocklightCol + vec3(0.0, -oldLightmap.x * 0.1, oldLightmap.x * 0.16);
-    blockLighting = clamp01(blockLighting);
+    vec3 blockLighting = lightmapXM * blocklightCol + 0.4 * vec3(0.0, -oldLightmap.x * 0.1, oldLightmap.x * 0.16);
 #else
     vec3 blockLighting = lightmapXM * blocklightCol;
 #endif
