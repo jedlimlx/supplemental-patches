cbd2eb6c66ef96d75fb159e0672a8b10a41b76c7
jedlimlx <jedlimlx@outlook.com>
2026-02-19 21:59:17 +0800

    Fix issue with Corviknight shader and particle shaders

--- src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/entity/cobblemon/corviknight.glsl
+++ src/main/resources/resourcepacks/builtin_shaders/assets/supplemental_patches/euphoria/entity/cobblemon/corviknight.glsl
@@ -1,1 +1,7 @@
-#include "/lib/materials/specificMaterials/terrain/anvil.glsl"
+if (color.r < 0.4) {
+    materialMask = OSIEBCA; // Intense Fresnel
+    #include "/lib/materials/specificMaterials/terrain/anvil.glsl"
+
+    smoothnessG *= 1.5;
+    smoothnessD *= 1.5;
+}
